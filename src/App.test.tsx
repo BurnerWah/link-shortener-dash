6df332815054d8d9ab4624d9846a1ddc90412c8f
Burner <burner@burner.gay>
2022-08-11 22:23:41 -0500

    Replaced default test with dummy test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Dummy Test, Please Ignore', () => expect(true).toBe(true))
