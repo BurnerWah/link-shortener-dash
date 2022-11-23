852d58fc485087f19663e182ede42ccdcaf2486b
Burner <burner@burner.gay>
2022-11-23 02:48:18 -0600

    Added qrcode vite plugin, disabled vitest checks as they aren't needed

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,19 @@ import react from '@vitejs/plugin-react'
 import postcssNormalize from 'postcss-normalize'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
+import { qrcode } from 'vite-plugin-qrcode'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    !process.env.VITEST &&
-      checker({
-        typescript: true,
-        eslint: {
-          lintCommand: 'eslint --cache "./src/**/*.{ts,tsx}"',
-        },
-      }),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint --cache "./src/**/*.{ts,tsx}"',
+      },
+    }),
+    qrcode(),
   ],
   css: {
     postcss: {
