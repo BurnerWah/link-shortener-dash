c0bafa3e9da0ed646d808eba4cdc5d6a0083fd00
Burner <burner@burner.gay>
2022-12-12 15:06:37 -0600

    Updated dependencies, removed some vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import react from '@vitejs/plugin-react'
 import postcssNormalize from 'postcss-normalize'
 import { defineConfig } from 'vite'
-import checker from 'vite-plugin-checker'
-import { qrcode } from 'vite-plugin-qrcode'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-      eslint: {
-        lintCommand: 'eslint --cache "./src/**/*.{ts,tsx}"',
-      },
-    }),
-    qrcode(),
-  ],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [postcssNormalize()],
